0b41da91313aab14e316f115705eff62f617a893
aacrabtree <46791328+aacrabtree@users.noreply.github.com>
2020-08-19 11:53:58 -0500

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,7 +30,15 @@ namespace API
             services.AddDbContext<DataContext>(opt => {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000")
+                );
+            });
             services.AddControllers();
+            services.AddMvc(option => option.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,11 +54,15 @@ namespace API
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+
+            
+            app.UseMvc();
         }
     }
 }
